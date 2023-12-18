b5bead314a64d065ef652b18a38df8ce1f6d7c0a
Anudeep <anudeepgowda02@gmail.com>
2023-12-18 15:34:42 +0530

    updates variables defaults

--- variables.tf
+++ variables.tf
@@ -6,11 +6,13 @@ variable "vpc_cidr" {
 variable "private_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
+  default = [] 
 }
 
 variable "public_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
+  default = [] 
 }
 
 variable "environment" {
